bfce56225b0c9e067cd8db21c77461bcf45d1611
JHS <wnghdtjr129@naver.com>
2023-08-28 16:33:29 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/main/java/com/ray/pomin/PominApplication.java
@@ -0,0 +1,13 @@
+package com.ray.pomin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PominApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PominApplication.class, args);
+	}
+
+}
