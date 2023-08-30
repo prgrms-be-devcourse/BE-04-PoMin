15072a420603d95406bada603cf348c254fbd16d
JHS <wnghdtjr129@naver.com>
2023-08-30 19:51:31 +0900

    feat: owner 저장 시 비밀번호 패턴 검증 및 암호화

--- src/main/java/com/ray/pomin/user/domain/Owner.java
+++ src/main/java/com/ray/pomin/user/domain/Owner.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.ray.pomin.global.util.validation.Validator.Condition.regex;
 import static com.ray.pomin.global.util.validation.Validator.validate;
@@ -16,6 +17,7 @@ import static lombok.AccessLevel.PROTECTED;
 public class Owner extends BaseEntity {
 
     private static final String EMAIL_PATTERN = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[a-zA-Z\\d])(?=.*?[?!@$]).{8,}$";
 
     @Id
     @GeneratedValue
@@ -27,13 +29,13 @@ public class Owner extends BaseEntity {
 
     private String email;
 
-    public Owner(String loginId, String password, String email) {
+    public Owner(String loginId, String password, String email, PasswordEncoder passwordEncoder) {
         validate(isNull(loginId), "로그인 아이디를 입력하세요");
-        validate(isNull(password), "비밀번호를 입력하세요");
+        validate(regex(PASSWORD_PATTERN, password), "비밀번호를 입력하세요");
         validate(regex(EMAIL_PATTERN, email), "이메일 형식에 맞게 작성하세요");
 
         this.loginId = loginId;
-        this.password = password;
+        this.password = passwordEncoder.encode(password);
         this.email = email;
     }
 
