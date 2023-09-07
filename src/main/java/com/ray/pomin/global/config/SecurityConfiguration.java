b95db322d16d491576a23b0f00206832401e526c
JHS <wnghdtjr129@naver.com>
2023-09-07 17:03:02 +0900

    feat: Security 설정 파일 추가

---
+++ src/main/java/com/ray/pomin/global/config/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.ray.pomin.global.config;
+
+import com.ray.pomin.global.auth.OAuthCustomerService;
+import com.ray.pomin.global.auth.OAuthSuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final OAuthCustomerService oAuthCustomerService;
+    private final OAuthSuccessHandler oAuthSuccessHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .headers(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .rememberMe(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry ->
+                        registry
+                                .anyRequest().permitAll()
+                )
+                .oauth2Login(configure ->
+                        configure.userInfoEndpoint(userConfig ->
+                                userConfig.userService(oAuthCustomerService))
+                                .successHandler(oAuthSuccessHandler)
+                );
+        return http.build();
+    }
+}
