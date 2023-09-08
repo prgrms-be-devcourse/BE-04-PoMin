0317ffe8c184e2de7de1260efef999fd406a9420
JHS <wnghdtjr129@naver.com>
2023-09-08 16:22:56 +0900

    feat: 존재하는 이메일에 대해 가입 진행

--- src/main/java/com/ray/pomin/global/auth/OAuthSuccessHandler.java
+++ src/main/java/com/ray/pomin/global/auth/OAuthSuccessHandler.java
@@ -1,6 +1,8 @@
 package com.ray.pomin.global.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ray.pomin.customer.domain.Customer;
+import com.ray.pomin.customer.repository.CustomerRepository;
 import com.ray.pomin.global.auth.model.OAuthCustomer;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -12,12 +14,14 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final ObjectMapper objectMapper;
+    private final CustomerRepository customerRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
@@ -25,9 +29,24 @@ public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
         response.setCharacterEncoding("UTF-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Optional<Customer> findCustomer = customerRepository.findByLoginEmail(oAuthCustomer.getRegistration().email());
+        if (findCustomer.isPresent()) {
+            Customer customer = findCustomer.get().addLoginProvider(oAuthCustomer.getRegistration().providerName());
+            customerRepository.save(customer);
+
+            response.getWriter().println(objectMapper.writeValueAsString(createJWT(customer)));
+
+            return;
+        }
+
         response.setHeader("Location", "/api/v1/customers");
 
         response.getWriter().println(objectMapper.writeValueAsString(oAuthCustomer.getRegistration()));
+    }
+
+    private String createJWT(Customer customer) {
+        return "accessToken";
     }
 
 }
