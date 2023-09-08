c88c5e7a01ef94c75bacb32f7d862e0895943f6a
bumwoo <gld55602@gmail.com>
2023-09-08 14:00:21 +0900

    feat: 주문, 주문항목 연관관계 추가

--- src/main/java/com/ray/pomin/order/Order.java
+++ src/main/java/com/ray/pomin/order/Order.java
@@ -1,14 +1,20 @@
 package com.ray.pomin.order;
 
 import com.ray.pomin.common.domain.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -22,6 +28,10 @@ public class Order extends BaseTimeEntity {
 
     @Embedded
     private OrderInfo orderInfo;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private List<OrderLineItem> orderLineItems = new ArrayList<>();
 
     public void place() {
         validateOrder();
