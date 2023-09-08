63db92f206af5983f18c393f1c03b7f2cdb92f67
JHS <wnghdtjr129@naver.com>
2023-09-08 16:15:52 +0900

    feat: login provider 분리

---
+++ src/main/java/com/ray/pomin/customer/domain/LoginProvider.java
@@ -0,0 +1,32 @@
+package com.ray.pomin.customer.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class LoginProvider {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Provider provider;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    public LoginProvider(Provider provider, Customer customer) {
+        this.provider = provider;
+        this.customer = customer;
+    }
+
+}
