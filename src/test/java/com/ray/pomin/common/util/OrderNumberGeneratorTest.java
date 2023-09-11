46cb84866431bfc08567a1ebdec7dca92d3fa477
bumwoo <gld55602@gmail.com>
2023-09-11 18:02:35 +0900

    test: orderNumber 정규표현식 포장

--- src/test/java/com/ray/pomin/common/util/OrderNumberGeneratorTest.java
+++ src/test/java/com/ray/pomin/common/util/OrderNumberGeneratorTest.java
@@ -7,12 +7,14 @@ public class OrderNumberGeneratorTest {
 
     public static final int RANDOM_DIGITS = 3;
 
+    public static final String ORDER_NUMBER_REGEX = "\\d+";
+
     @Test
     public void testGenerateOrderNumber() {
         String orderNumber = OrderNumberGenerator.generateOrderNumber();
         assertNotNull(orderNumber);
         assertEquals(RANDOM_DIGITS + 13, orderNumber.length());
-        assertTrue(orderNumber.matches("\\d+"));
+        assertTrue(orderNumber.matches(ORDER_NUMBER_REGEX));
     }
 
 }
