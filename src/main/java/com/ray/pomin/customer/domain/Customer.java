ce27b41b62c1f359765d33d8fb992104a8de65e3
JHS <wnghdtjr129@naver.com>
2023-09-11 12:38:25 +0900

    refactor: validate logic 묶기

--- src/main/java/com/ray/pomin/customer/domain/Customer.java
+++ src/main/java/com/ray/pomin/customer/domain/Customer.java
@@ -38,15 +38,19 @@ public class Customer {
     private List<LoginProvider> loginProviders = new ArrayList<>();
 
     public Customer(Login login, String nickname, CustomerInfo information, Provider provider) {
-        validate(!isNull(login), "로그인 정보는 필수 값입니다");
-        validate(hasContent(nickname), "닉네임은 필수 값입니다");
-        validate(!isNull(information), "사용자 정보는 필수 값입니다");
-        validate(!isNull(provider), "로그인 제공자 정보는 필수 값입니다");
+        validateCustomer(login, nickname, information, provider);
 
         this.login = login;
         this.nickname = nickname;
         this.information = information;
         this.loginProviders.add(new LoginProvider(provider, this));
+    }
+
+    private static void validateCustomer(Login login, String nickname, CustomerInfo information, Provider provider) {
+        validate(!isNull(login), "로그인 정보는 필수 값입니다");
+        validate(hasContent(nickname), "닉네임은 필수 값입니다");
+        validate(!isNull(information), "사용자 정보는 필수 값입니다");
+        validate(!isNull(provider), "로그인 제공자 정보는 필수 값입니다");
     }
 
     public String getEmail() {
