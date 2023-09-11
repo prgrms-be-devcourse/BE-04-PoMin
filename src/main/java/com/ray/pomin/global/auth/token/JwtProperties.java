25b853c6b6a15eb88748a511e3cc1326f985af3c
JHS <wnghdtjr129@naver.com>
2023-09-11 17:35:07 +0900

    feat: properties 값 저장객체 생성

---
+++ src/main/java/com/ray/pomin/global/auth/token/JwtProperties.java
@@ -0,0 +1,22 @@
+package com.ray.pomin.global.auth.token;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
+
+@ConfigurationProperties("jwt")
+public record JwtProperties(String secretKey, Map<String, TokenInfo> info) {
+
+    public long getExpiration(String tokenName) {
+        return info.get(tokenName).expiration;
+    }
+
+    private record TokenInfo(int expiration, String headerName) {
+
+        public TokenInfo(int expiration, String headerName) {
+            this.expiration = expiration * 1000;
+            this.headerName = headerName;
+        }
+
+    }
+}
