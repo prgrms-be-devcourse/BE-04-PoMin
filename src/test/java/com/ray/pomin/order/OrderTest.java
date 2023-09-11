3b99342efe9a5e579d38e5c15b07c7b7a0f6e680
bumwoo <gld55602@gmail.com>
2023-09-11 17:29:09 +0900

    test: Order 테스트코드 작성

---
+++ src/test/java/com/ray/pomin/order/OrderTest.java
@@ -0,0 +1,51 @@
+package com.ray.pomin.order;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class OrderTest {
+
+    private Order order;
+
+    @BeforeEach
+    public void setUp() {
+        order = new Order();
+    }
+
+    @Test
+    public void testPlaceOrder() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            order.place();
+        });
+
+        order.getOrderItems().add(new OrderItem());
+        order.place();
+
+        assertEquals(OrderStatus.CREATED, order.getOrderStatus());
+    }
+
+    @Test
+    public void testPaidOrder() {
+        order.paid();
+
+        assertEquals(OrderStatus.PAID, order.getOrderStatus());
+    }
+
+    @Test
+    public void testOrderValidation() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            order.validateOrder();
+        });
+
+        order.getOrderItems().add(new OrderItem());
+
+        assertDoesNotThrow(() -> {
+            order.validateOrder();
+        });
+    }
+
+}
