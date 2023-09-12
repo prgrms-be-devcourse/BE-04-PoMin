c81db722d77b7bf190174401170f1bdc87201f0b
bumwoo <gld55602@gmail.com>
2023-09-12 14:52:44 +0900

    chore: toOrder inline 수정

--- src/main/java/com/ray/pomin/order/Cart.java
+++ src/main/java/com/ray/pomin/order/Cart.java
@@ -1,10 +1,6 @@
 package com.ray.pomin.order;
 
-import org.aspectj.weaver.ast.Or;
-import org.springframework.security.core.userdetails.User;
-
 import java.util.List;
-import java.util.Locale;
 
 public record Cart(
         Long customerId,
@@ -13,11 +9,10 @@ public record Cart(
 
 ) {
     public Order toOrder() {
-        Order order = new Order().builder()
+        return new Order().builder()
                 .customerId(customerId)
                 .orderItems(orderItems)
                 .storeId(storeId)
                 .build();
-        return order;
     }
 }
