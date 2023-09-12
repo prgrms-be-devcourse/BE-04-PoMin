9890d1d29e869ffb86ce0c6fdc7976f965182d70
bumwoo <gld55602@gmail.com>
2023-09-12 13:04:37 +0900

    test: 개행, 매직넘버 포장

--- src/test/java/com/ray/pomin/common/util/OrderNumberGeneratorTest.java
+++ src/test/java/com/ray/pomin/common/util/OrderNumberGeneratorTest.java
@@ -8,12 +8,14 @@ public class OrderNumberGeneratorTest {
     public static final int RANDOM_DIGITS = 3;
 
     public static final String ORDER_NUMBER_REGEX = "\\d+";
+    public static final int ORDER_NUMBER_LENGTH = 13;
 
     @Test
     public void testGenerateOrderNumber() {
         String orderNumber = OrderNumberGenerator.generateOrderNumber();
+
         assertNotNull(orderNumber);
-        assertEquals(RANDOM_DIGITS + 13, orderNumber.length());
+        assertEquals(RANDOM_DIGITS + ORDER_NUMBER_LENGTH, orderNumber.length());
         assertTrue(orderNumber.matches(ORDER_NUMBER_REGEX));
     }
 
