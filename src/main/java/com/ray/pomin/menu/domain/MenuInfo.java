c86db34253b2101e6890a4501dfc14be68cb8c62
JHS <wnghdtjr129@naver.com>
2023-09-13 15:38:57 +0900

    feat: menu domain 수정

---
+++ src/main/java/com/ray/pomin/menu/domain/MenuInfo.java
@@ -0,0 +1,27 @@
+package com.ray.pomin.menu.domain;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Embeddable
+@EqualsAndHashCode
+@NoArgsConstructor(access = PROTECTED)
+public class MenuInfo {
+
+    @Embedded
+    private PrimaryDetails primaryDetails;
+
+    @Embedded
+    private AdditionalDetails additionalDetails;
+
+    public MenuInfo(PrimaryDetails primaryDetails, AdditionalDetails additionalDetails) {
+        this.primaryDetails = primaryDetails;
+        this.additionalDetails = additionalDetails;
+    }
+}
---
+++ src/main/java/com/ray/pomin/menu/domain/MenuOption.java
@@ -0,0 +1,6 @@
+package com.ray.pomin.menu.domain;
+
+public enum MenuOption {
+    BEST_SELLER,
+    POPULAR_ITEM;
+}
