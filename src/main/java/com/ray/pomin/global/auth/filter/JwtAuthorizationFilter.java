658f8b12ef3cbf3f6c2c7666965fd2b397f9929e
JHS <wnghdtjr129@naver.com>
2023-09-14 13:53:01 +0900

    fix: 테스트 시 JWTFilter 불러오는 문제 해결

--- src/main/java/com/ray/pomin/global/auth/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ray/pomin/global/auth/filter/JwtAuthorizationFilter.java
@@ -11,13 +11,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
--- src/main/java/com/ray/pomin/global/config/SecurityBeanConfiguration.java
+++ src/main/java/com/ray/pomin/global/config/SecurityBeanConfiguration.java
@@ -1,5 +1,8 @@
 package com.ray.pomin.global.config;
 
+import com.ray.pomin.global.auth.filter.JwtAuthorizationFilter;
+import com.ray.pomin.global.auth.token.JwtProperties;
+import com.ray.pomin.global.auth.token.JwtService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +12,11 @@ public class SecurityBeanConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(JwtService jwtService, JwtProperties properties) {
+        return new JwtAuthorizationFilter(jwtService, properties);
     }
 
 }
