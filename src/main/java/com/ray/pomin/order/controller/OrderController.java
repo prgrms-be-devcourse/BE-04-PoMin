c971552a224d48d661ba77280e7190c4c3092d66
bumwoo <gld55602@gmail.com>
2023-09-15 15:14:02 +0900

    feat: OrderController dto 반환

--- src/main/java/com/ray/pomin/order/controller/OrderController.java
+++ src/main/java/com/ray/pomin/order/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.ray.pomin.order.controller;
 
 import com.ray.pomin.order.Cart;
 import com.ray.pomin.order.Order;
+import com.ray.pomin.order.controller.dto.OrderResponse;
 import com.ray.pomin.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -21,17 +23,19 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/orders")
-    public Order saveOrder(@RequestBody Cart cart) {
+    public OrderResponse saveOrder(@RequestBody Cart cart) {
         Order order = cart.toOrder();
         orderService.createOrder(order);
         payOrder(order);
-        return order;
+        return new OrderResponse(order);
     }
 
     @GetMapping("/orders/{customerId}")
-    public List<Order> getOrdersByCustomerId(@PathVariable Long customerId) {
+    public List<OrderResponse> getOrdersByCustomerId(@PathVariable Long customerId) {
         List<Order> orders = orderService.getOrdersByCustomerId(customerId);
-        return orders;
+        return orders.stream()
+                .map(OrderResponse::new)
+                .collect(Collectors.toList());
     }
 
     private void payOrder(Order order) {
