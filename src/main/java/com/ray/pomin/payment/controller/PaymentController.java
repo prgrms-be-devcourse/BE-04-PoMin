77b88b287c04bbb3bd087fd115b6f00c8341d4ed
ddongpuri <fnffnfkffk1112@gmail.com>
2023-09-18 17:22:28 +0900

    chore: 미사용 매핑 제거

--- src/main/java/com/ray/pomin/payment/controller/PaymentController.java
+++ src/main/java/com/ray/pomin/payment/controller/PaymentController.java
@@ -1,12 +1,10 @@
 package com.ray.pomin.payment.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.ray.pomin.payment.controller.dto.PaymentFailResponse;
 import com.ray.pomin.payment.controller.dto.PaymentResponse;
 import com.ray.pomin.payment.domain.Payment;
 import com.ray.pomin.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,9 +15,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.net.URI;
-
-@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class PaymentController {
@@ -33,16 +28,6 @@ public class PaymentController {
   }
 
   @ResponseBody
-  @GetMapping("/payment/success")
-  public ResponseEntity<Void> create(@RequestParam String orderId,
-                                            @RequestParam String paymentKey,
-                                            @RequestParam int amount) throws JsonProcessingException {
-    Long paymentId = paymentService.create(orderId, paymentKey, amount);
-
-    return ResponseEntity.created(URI.create("/api/v1/payments" + paymentId)).build();
-  }
-
-  @ResponseBody
   @GetMapping("/payment/fail")
   public ResponseEntity<PaymentFailResponse> fail(@RequestParam String code,
                                                   @RequestParam String message,
@@ -51,7 +36,7 @@ public class PaymentController {
   }
 
   @PatchMapping("/payments/{paymentId}")
-  public ResponseEntity<Void> cancel(@PathVariable Long paymentId) throws JsonProcessingException {
+  public ResponseEntity<Void> cancel(@PathVariable Long paymentId) {
     Payment paymentToCancel = paymentService.findOne(paymentId);
     paymentService.cancel(paymentToCancel);
 
