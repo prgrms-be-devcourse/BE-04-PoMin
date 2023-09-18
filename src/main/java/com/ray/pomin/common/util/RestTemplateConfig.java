8977e5af6d281e054d8ab8bd09d2b411f1059ff4
ddongpuri <fnffnfkffk1112@gmail.com>
2023-09-18 17:30:18 +0900

    feat: restTemplate timeout 및 재시도 설정, log 설정

--- src/main/java/com/ray/pomin/common/util/RestTemplateConfig.java
+++ src/main/java/com/ray/pomin/common/util/RestTemplateConfig.java
@@ -1,16 +1,75 @@
 package com.ray.pomin.common.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.stream.Collectors;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(5))
+                .setReadTimeout(Duration.ofSeconds(5))
+                .additionalInterceptors(clientHttpRequestInterceptor(), new LoggingInterceptor())
+                .requestFactory(() -> new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()))
+                .build();
+    }
+
+    public ClientHttpRequestInterceptor clientHttpRequestInterceptor() {
+        return (request, body, execution) -> {
+            RetryTemplate retryTemplate = new RetryTemplate();
+            retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3));
+            try {
+                return retryTemplate.execute(context -> execution.execute(request, body));
+            } catch (Throwable throwable) {
+                throw new RuntimeException(throwable);
+            }
+        };
+    }
+
+    @Slf4j
+    static class LoggingInterceptor implements ClientHttpRequestInterceptor {
+
+        @Override
+        public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+            printRequest(request, body);
+            ClientHttpResponse response = execution.execute(request, body);
+            printResponse(response);
+
+            return response;
+        }
+
+        private void printRequest(final HttpRequest req, final byte[] body) {
+            log.info("URI: {}, Method: {}, Headers:{}, Body:{} ",
+                    req.getURI(), req.getMethod(), req.getHeaders(), new String(body, StandardCharsets.UTF_8));
+        }
+
+        private void printResponse(final ClientHttpResponse res) throws IOException {
+            String body = new BufferedReader(new InputStreamReader(res.getBody(), StandardCharsets.UTF_8)).lines()
+                    .collect(Collectors.joining("\n"));
+
+            log.info("Status: {}, Headers:{}, Body:{} ",
+                   res.getStatusCode(), res.getHeaders(), body);
+        }
     }
 
 }
