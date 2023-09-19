8b00ad66e666c60870d9f7031035c769d19b936e
JHS <wnghdtjr129@naver.com>
2023-09-19 17:16:58 +0900

    fix: 조회시 연관관계 같이 조회

--- src/main/java/com/ray/pomin/customer/repository/CustomerRepository.java
+++ src/main/java/com/ray/pomin/customer/repository/CustomerRepository.java
@@ -1,12 +1,14 @@
 package com.ray.pomin.customer.repository;
 
 import com.ray.pomin.customer.domain.Customer;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    @EntityGraph(attributePaths = "loginProviders")
     Optional<Customer> findByLoginEmail(String email);
 
 }
