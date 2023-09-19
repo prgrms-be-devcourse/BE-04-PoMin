39f5e451b2196452fde786d5fdeb04134321bfa3
bumwoo <gld55602@gmail.com>
2023-09-19 17:00:16 +0900

    feat: OrderController 수정

--- src/main/java/com/ray/pomin/order/controller/OrderController.java
+++ src/main/java/com/ray/pomin/order/controller/OrderController.java
@@ -1,22 +1,24 @@
 package com.ray.pomin.order.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ray.pomin.customer.controller.dto.OrderRequest;
 import com.ray.pomin.global.auth.model.Claims;
 import com.ray.pomin.order.Cart;
 import com.ray.pomin.order.Order;
+import com.ray.pomin.order.OrderInfo;
 import com.ray.pomin.order.controller.dto.OrderResponse;
 import com.ray.pomin.order.service.OrderService;
 import com.ray.pomin.payment.domain.Payment;
 import com.ray.pomin.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
@@ -24,7 +26,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
 
@@ -32,7 +34,7 @@ public class OrderController {
 
     private final PaymentService paymentService;
 
-    @PostMapping("/orders")
+    @PostMapping()
     public OrderResponse saveOrder(@RequestBody Cart cart, @RequestParam String paymentKey) {
         Order order = cart.toOrder();
         orderService.createOrder(order);
@@ -41,7 +43,7 @@ public class OrderController {
     }
 
 
-    @GetMapping("/orders")
+    @GetMapping()
     public List<OrderResponse> getOrdersByCustomerId(@AuthenticationPrincipal Claims claims) {
         List<Order> orders = orderService.getOrdersByCustomerId(claims.id());
         return orders.stream()
@@ -49,7 +51,7 @@ public class OrderController {
                 .collect(Collectors.toList());
     }
 
-    @GetMapping("/orders/payOrder")
+    @GetMapping("/payOrder")
     public ResponseEntity<Void> create(@RequestParam String orderNumber,
                                        @RequestParam String paymentKey,
                                        @RequestParam int amount) {
@@ -58,6 +60,15 @@ public class OrderController {
         payOrder(order, paymentKey);
         return ResponseEntity.created(URI.create("/api/v1/payments" + paymentId)).build();
     }
+
+    @PostMapping("{orderNumber}")
+    public ResponseEntity<OrderRequest> acceptOrder(@PathVariable String orderNumber, @RequestBody OrderRequest orderRequest) {
+        Order order = orderService.getOrderByOrderNumber(orderNumber);
+        OrderInfo orderInfo = OrderInfo.acceptOrderInfo(orderNumber, orderRequest);
+        orderService.acceptOrder(order, orderInfo);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
     private void payOrder(Order order, String paymentKey) {
         Payment payment = paymentService.findByPgInfoPayKey(paymentKey);
