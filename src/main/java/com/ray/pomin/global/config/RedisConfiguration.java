06ad5f6063062175f4fd9fa9a2a22d48a39caa8f
JHS <wnghdtjr129@naver.com>
2023-09-20 15:51:43 +0900

    feat: mail redis db 커넥션 설정

--- src/main/java/com/ray/pomin/global/config/RedisConfiguration.java
+++ src/main/java/com/ray/pomin/global/config/RedisConfiguration.java
@@ -4,7 +4,9 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
@@ -14,6 +16,22 @@ public class RedisConfiguration {
     @Bean
     public RedisConnectionFactory redisConnectionFactory(RedisProperties properties) {
         return new LettuceConnectionFactory(properties.getHost(), properties.getPort());
+    }
+
+    @Bean
+    public RedisConnectionFactory mailRedisConnectionFactory(RedisProperties properties) {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration(properties.getHost(), properties.getPort());
+        configuration.setDatabase(1);
+
+        return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory mailRedisConnectionFactory) {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(mailRedisConnectionFactory);
+
+        return template;
     }
 
 }
