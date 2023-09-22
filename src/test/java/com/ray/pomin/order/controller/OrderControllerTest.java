838b24d341438b85ca59f947b493e3079ece603f
bumwoo <gld55602@gmail.com>
2023-09-22 17:19:58 +0900

    test: OrderControllerTest 작성

---
+++ src/test/java/com/ray/pomin/order/controller/OrderControllerTest.java
@@ -0,0 +1,91 @@
+package com.ray.pomin.order.controller;
+
+import com.ray.pomin.common.domain.Point;
+import com.ray.pomin.customer.controller.base.ControllerUnit;
+import com.ray.pomin.order.Cart;
+import com.ray.pomin.order.Order;
+import com.ray.pomin.order.service.OrderService;
+import com.ray.pomin.payment.service.PaymentService;
+import com.ray.pomin.store.controller.dto.StoreSaveRequest;
+import com.ray.pomin.store.domain.Store;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrderController.class)
+public class OrderControllerTest extends ControllerUnit {
+
+    @MockBean
+    private OrderService orderService;
+
+    @MockBean
+    private PaymentService paymentService;
+
+    private Store store;
+
+    @BeforeEach
+    void setUp() {
+        StoreSaveRequest request = new StoreSaveRequest(
+                1L,
+                "storeName",
+                "010-1234-5678",
+                "서울시 강남구 역삼동",
+                "12-234",
+                "12:00",
+                "22:00",
+                List.of()
+        );
+        Point point = new Point(34.123, 123.245);
+        store = request.toEntity(point);
+    }
+
+    @Test
+    void saveOrder() throws Exception {
+        Cart cart = new Cart(1L, List.of(), store);
+
+        ResultActions resultActions = mvc.perform(RestDocumentationRequestBuilders.post("/api/v1/orders")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(cart)));
+
+        resultActions
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(
+                        document("order/save",
+                                responseFields(
+                                        fieldWithPath("id").type(JsonFieldType.NULL).description("주문 ID"),
+                                        fieldWithPath("store").type(JsonFieldType.OBJECT).description("가게 정보"),
+                                        fieldWithPath("store.name").type(JsonFieldType.STRING).description("가게 이름"),
+                                        fieldWithPath("store.createdDate").type(JsonFieldType.NULL).description("가게 이름"),
+                                        fieldWithPath("store.lastModifiedDate").type(JsonFieldType.NULL).description("가게 이름"),
+                                        fieldWithPath("store.phoneNumber").type(JsonFieldType.STRING).description("가게 이름"),
+                                        fieldWithPath("store.time").type(JsonFieldType.OBJECT).description("가게 이름"),
+                                        fieldWithPath("store.time.open").type(JsonFieldType.STRING).description("가게 이름"),
+                                        fieldWithPath("store.time.close").type(JsonFieldType.STRING).description("가게 이름"),
+                                        fieldWithPath("store.storeImages").type(JsonFieldType.ARRAY).description("가게 이름"),
+                                        fieldWithPath("orderInfo").type(JsonFieldType.NULL).description("주문 정보"),
+                                        fieldWithPath("totalPrice").type(JsonFieldType.NUMBER).description("주문 총 가격"),
+                                        fieldWithPath("payment").type(JsonFieldType.NULL).description("결제 정보")
+                                        )
+                        )
+                );
+
+    }
+
+}
