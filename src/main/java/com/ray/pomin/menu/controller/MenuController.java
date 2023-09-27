462c7b3ac42dace757a9e8cb095e913ff9516993
HongSeok Ju <wnghdtjr129@naver.com>
2023-09-27 18:17:20 +0900

    fix: api prefis 수정

--- src/main/java/com/ray/pomin/menu/controller/MenuController.java
+++ src/main/java/com/ray/pomin/menu/controller/MenuController.java
@@ -14,7 +14,7 @@ public class MenuController {
 
     private final MenuService menuService;
 
-    @PostMapping("/menus")
+    @PostMapping("/api/v1/menus")
     public ResponseEntity<Void> saveMenu(@RequestBody SaveMenuRequest request) {
         menuService.save(request.toEntity());
 
